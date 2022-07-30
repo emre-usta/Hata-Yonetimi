d1ef04f8f0076e25f3d929bfb299892c7c3e386c
emre-usta <78980667+emre-usta@users.noreply.github.com>
2022-07-30 20:46:09 +0300

    HataYonetimi

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* try
+            {
+                Console.WriteLine("Bir Sayi Giriniz...");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğinşz sayi: " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Islem tamamlandı...");
+            } */
+
+            try
+            {
+                //int a = int.Parse(null);
+                int a = int.Parse("test");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                 Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+        }   
+    }
+}
